da4805f40a477c44c27c8801f9d4204c1010961b
Harshit Singh <153013485+harshit8786@users.noreply.github.com>
2026-01-12 16:52:08 +0530

    Remove reverse words implementation

--- reverse_words.js
+++ reverse_words.js
@@ -1,7 +1,1 @@
-let sentence = "Java is fun";
-let words = sentence.split(" ");
-let result = " ";
-for (let i = words.length - 1; i >= 0; i--) {
-    result = result + words[i] + " ";
-}
-console.log(result.trim());
+
