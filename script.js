ce8d0490ae4dba1adc1349e9005595a380d32b12
Harshit Singh <harshitsingh@Harshits-MacBook-Air-2.local>
2026-01-15 14:52:50 +0530

    Started JS practice

---
+++ script.js
@@ -0,0 +1,16 @@
+//1 HEllo world
+console.log("Node.js Backend Started");
+// 2 Variables
+let name = "Aman";    // String
+let age = 22;         // Number
+const country = "India"; // Constant
+let isStudent = true; // Boolean
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isStudent);
+
+// Data Types Examples
+let score; // Undefined
+let data = null; // Null
